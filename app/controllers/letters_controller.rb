d5340997568c8d915b26bf45c15e140e43afd69d
eguitarz <dalema22@gmail.com>
2014-01-24 21:38:53 +0800

    send edit and deleteable for letters

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,10 +1,37 @@
 class LettersController < ApplicationController
+	API_KEY = ENV['MAILGUN_KEY']
+	API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/listone.im/messages"
+
 	def index
 		@letters = Letter.all
 	end
 
 	def show
-		@letter = Letter.find(parmas[:id])
+		@letter = Letter.find(params[:id])
+		@url = API_URL
+	end
+
+	def edit
+		@letter = Letter.find(params[:id])
+	end
+
+	def update
+		@letter = Letter.find(params[:id])
+		flash[:notice] = flash[:error] = ''
+		if @letter.update(letter_params)
+			flash[:notice] = 'updated'
+		else
+			flash[:error] = 'failed to update'
+		end
+
+		redirect_to letter_path(params[:id])
+	end
+
+	def destroy
+		@letter = Letter.find(params[:id])
+		@letter.destroy()
+
+		redirect_to :letters
 	end
 
 	def new
@@ -12,6 +39,19 @@ class LettersController < ApplicationController
 	end
 
 	def create
-		@letter = Letter.create(params[:letter])
+		@letter = Letter.new(letter_params)
+		flash[:notice] = flash[:error] = ''
+		if @letter.save
+			flash[:notice] = 'created a mail'
+		else
+			flash[:error] = 'failed to create mails'
+		end
+
+		redirect_to :letters
+	end
+
+	private
+	def letter_params
+		params.require(:letter).permit(:title, :from, :to, :cc, :bcc, :campaign_id, :body)
 	end
 end
