889eea20113bae93cfafad55a25a4d43f54c61f1
eguitarz <dalema22@gmail.com>
2014-01-30 21:06:37 +0800

    set root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 MailgunRails::Application.routes.draw do
   resources :letters
+  root 'letters#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
